650bed0bab40e9708806065a22f319e2f5abe77a
Maria Kuklina <mkdirwtf@gmail.com>
2019-09-15 20:09:47 +0300

    Impl an evaluator of SM and a compiler from stmt to SM

--- src/SM.ml
+++ src/SM.ml
@@ -17,13 +17,54 @@ type prg = insn list
  *)
 type config = int list * Syntax.Stmt.config
 
+
 (* Stack machine interpreter
 
      val eval : config -> prg -> config
 
    Takes a configuration and a program, and returns a configuration as a result
  *)                         
-let eval _ = failwith "Not yet implemented"
+(* let eval _ = failwith "Not yet implemented" *)
+
+let matchOp = Syntax.Expr.matchOp
+
+let binop (stack, c) op =
+  match stack with
+    | x :: y :: stack' -> ((matchOp op x y) :: stack', c)
+    | _ -> failwith "Unable to apply binary operation: too few elements in the stack."
+ 
+let const (stack, c) x = (x :: stack, c)
+
+let read (stack, (s, input, o)) = 
+  match input with
+    | i :: ins -> (i :: stack, (s, ins, o))
+    | _ -> failwith "Unable to read from the input: empty input."
+
+let write (stack, (state, i, output)) =
+  match stack with
+    | s :: stack' -> (stack', (state, i, output @ [s]))
+    | _  -> failwith "Unable to write to the output: empty stack."
+
+let load (stack, (state, i, o)) v = ((state v) :: stack, (state, i, o))
+
+let store (stack, (state, i, o)) v =
+  match stack with
+    | s :: stack' -> (stack', (Syntax.Expr.update v s state, i, o))
+    | _ -> failwith "Unable to store a value: empty stack."
+
+let eval_inst config inst = 
+  match inst with
+    | BINOP op -> binop config op
+    | CONST x  -> const config x
+    | READ     -> read config
+    | WRITE    -> write config
+    | LD    v  -> load config v
+    | ST    v  -> store config v
+
+let rec eval config prog =
+  match prog with
+    | inst :: insts -> eval (eval_inst config inst) insts
+    | _ -> config
 
 (* Top-level evaluation
 
@@ -41,4 +82,15 @@ let run i p = let (_, (_, _, o)) = eval ([], (Syntax.Expr.empty, i, [])) p in o
    stack machine
  *)
 
-let compile _ = failwith "Not yet implemented"
+let rec compile_expr expr =
+  match expr with
+    | Syntax.Expr.Const x -> [CONST x]
+    | Syntax.Expr.Var v -> [LD v]
+    | Syntax.Expr.Binop (op, expr1, expr2) -> compile_expr expr1 @ compile_expr expr2 @ [BINOP op]
+
+let rec compile stmt =
+  match stmt with
+    | Syntax.Stmt.Read v             -> [READ; ST v]
+    | Syntax.Stmt.Write expr         -> compile_expr expr @ [WRITE]
+    | Syntax.Stmt.Assign (v, expr)   -> compile_expr expr @ [ST v]
+    | Syntax.Stmt.Seq (stmt1, stmt2) -> compile stmt1 @ compile stmt2
